9d4e3816ca839a034c1c1bb3f97ebb00b6651c67
whsu0202 <iimixhsu@gmail.com>
2019-02-08 23:03:16 +0800

    Moved all configurations to a file

---
+++ TestCases/config.py
@@ -0,0 +1,3 @@
+login_url = "http://www.amazon.com/"
+username = "20190208@yopmail.com"
+password = "accfortest0208"
--- TestCases/test_amazon_login_function_01.py
+++ TestCases/test_amazon_login_function_01.py
@@ -1,18 +1,10 @@
 import unittest
+import config
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
-import time
-
-config = {
-    'EMAIL': '20190208@yopmail.com',
-    'PASSWORD': 'accfortest0208'
-}
-
-login_url = 'http://www.amazon.com/'
-
 
 class SignInOut(unittest.TestCase):
 
@@ -21,7 +13,7 @@ class SignInOut(unittest.TestCase):
 
     def test_sign_in_function(self):
         driver = self.driver
-        driver.get(login_url)
+        driver.get(config.login_url)
         # Wait for data toaster to appear, then click it
         data_toaster = WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, "//*[@id='nav-main']/div[1]/div[2]/div/div[3]/span[1]/span")))
         data_toaster.click()
@@ -29,9 +21,9 @@ class SignInOut(unittest.TestCase):
         sign_in_button = driver.find_element_by_id('nav-link-accountList')
         sign_in_button.click()
         # Enter account
-        driver.find_element_by_id('ap_email').send_keys(config['EMAIL'])
+        driver.find_element_by_id('ap_email').send_keys(config.username)
         # Enter password
-        driver.find_element_by_id('ap_password').send_keys(config['PASSWORD'])
+        driver.find_element_by_id('ap_password').send_keys(config.password)
 	    # Click submit button (id=signInSubmit)
         driver.find_element_by_id('signInSubmit').click()
 	    # Find user info
